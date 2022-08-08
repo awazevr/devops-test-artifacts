c8a8fff1231cebc13cebef736811d56da62d563c
Sebastian Bjerre <sebastian.bjerre@novasol.com>
2022-08-08 14:40:09 +0200

    Initial commit

---
+++ src/main/java/com/example/devopsartifact/DevopsArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsArtifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsArtifactApplication.class, args);
+    }
+
+}
